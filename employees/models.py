452c0d2098a53a8d40247a4b50e23dd1ae791bb1
paffel2 <paffel2@gmail.com>
2025-04-22 11:21:40 +0300

    fix models and reqa

--- employees/models.py
+++ employees/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from employees.managers import CustomUserManager
 
-# Create your models here.
-
-
 
 class Department(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
-    description = models.TextField(blank=True, null=True, verbose_name="Описание департамента")
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание департамента"
+    )
 
     class Meta:
         verbose_name = "Отдел"
@@ -16,9 +15,10 @@ class Department(models.Model):
         ordering = ["id"]
 
     def __str__(self):
-            if self.name:
-                return f"Отдел {self.name}"
-            return f"Отдел {self.id}"
+        if self.name:
+            return f"Отдел {self.name}"
+        return f"Отдел {self.id}"
+
 
 class Position(models.Model):
     title = models.CharField(max_length=100, verbose_name="Название должности")
@@ -30,19 +30,22 @@ class Position(models.Model):
         ordering = ["id"]
 
     def __str__(self):
-            if self.title:
-                return f"Должность {self.title}"
-            return f"Должность {self.id}"
+        if self.title:
+            return f"Должность {self.title}"
+        return f"Должность {self.id}"
 
 
 class Employee(AbstractUser):
-    fullname = models.CharField(max_length=128,verbose_name="Полное имя")
+    fullname = models.CharField(max_length=128, verbose_name="Полное имя")
     email = models.EmailField(verbose_name="Электронная почта", unique=True)
     phone = models.CharField(max_length=20, verbose_name="Номер телефона")
-    position = models.ForeignKey(Position, on_delete=models.SET_NULL,null=True, blank=True)
-    department = models.ForeignKey(Department, on_delete=models.SET_NULL,null=True, blank=True)
-    hire_date = models.DateField(verbose_name="Дата найма",null=True,blank=True)
-    photo = models.ImageField(upload_to='employees/', blank=True, null=True)
+    position = models.ForeignKey(
+        Position, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    hire_date = models.DateField(verbose_name="Дата найма", null=True, blank=True)
     is_head = models.BooleanField(verbose_name="Глава департамента", default=False)
 
     USERNAME_FIELD = "email"
--- orders/models.py
+++ orders/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from employees.models import Employee
 
+
 class Order(models.Model):
     ORDER_TYPES = [
-        ('dismiss', 'Увольнение'),
-        ('vacation', 'Отпуск'),
-        ('hire', 'Прием на работу'),
-        ('sickleave',"Больничный")
+        ("dismiss", "Увольнение"),
+        ("vacation", "Отпуск"),
+        ("hire", "Прием на работу"),
+        ("sickleave", "Больничный"),
     ]
     number = models.CharField(max_length=50, verbose_name="Номер приказа")
-    order_type = models.CharField(max_length=50, choices=ORDER_TYPES,verbose_name="Тип приказа")
+    order_type = models.CharField(
+        max_length=50, choices=ORDER_TYPES, verbose_name="Тип приказа"
+    )
     date = models.DateField(auto_now_add=True, verbose_name="Дата издания приказа")
-    script_data = models.DateField(verbose_name="Дата подписания",null=True,blank=True)
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE, verbose_name="Сотрудник")
-    basis = models.TextField(null=True,blank=True, verbose_name="Основание")
-
+    script_data = models.DateField(
+        verbose_name="Дата подписания", null=True, blank=True
+    )
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, verbose_name="Сотрудник"
+    )
+    basis = models.TextField(null=True, blank=True, verbose_name="Основание")
 
     class Meta:
         verbose_name = "Приказ"
@@ -25,21 +31,25 @@ class Order(models.Model):
         if self.number:
             return f"Приказ {self.number}"
         return f"Приказ {self.id}"
-    
+
     def save(self, *args, **kwargs):
         is_new = self.pk is None
         super().save(*args, **kwargs)
-        
-        if self.order_type == 'dismiss' and is_new:
+
+        if self.order_type == "dismiss" and is_new:
             self.employee.is_active = False
             self.employee.save()
 
+
 class Vacation(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE,verbose_name="Сотрудник")
-    start_date = models.DateField(verbose_name="Дата начала",null=True,blank=True)
-    end_date = models.DateField(verbose_name="Дата окончания",null=True,blank=True)
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, verbose_name="Сотрудник"
+    )
+    start_date = models.DateField(verbose_name="Дата начала", null=True, blank=True)
+    end_date = models.DateField(verbose_name="Дата окончания", null=True, blank=True)
     vacation_type = models.CharField(max_length=50, verbose_name="Комментарий")
     is_paid = models.BooleanField(default=True, verbose_name="Оплачиваемый отпуск")
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Приказ")
 
     class Meta:
         verbose_name = "Отпуск"
@@ -48,10 +58,15 @@ class Vacation(models.Model):
 
 
 class SickLeave(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.CASCADE,verbose_name="Сотрудник")
-    start_date = models.DateField(verbose_name="Дата начала",null=True,blank=True)
-    end_date = models.DateField(verbose_name="Дата окончания",null=True,blank=True)
-    sick_number = models.CharField(max_length=50, verbose_name="Номер больничного листа")
+    employee = models.ForeignKey(
+        Employee, on_delete=models.CASCADE, verbose_name="Сотрудник"
+    )
+    start_date = models.DateField(verbose_name="Дата начала", null=True, blank=True)
+    end_date = models.DateField(verbose_name="Дата окончания", null=True, blank=True)
+    sick_number = models.CharField(
+        max_length=50, verbose_name="Номер больничного листа"
+    )
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="Приказ")
 
     class Meta:
         verbose_name = "Больничный"
